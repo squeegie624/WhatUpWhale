65eaa9ecbfa10ff4bfeeac2ea560216c1b5c6633
squeegie624 <squeegie624@gmail.com>
2017-09-07 20:34:18 -0600

    Add files via upload
    The footer and template file are in a folder called inc on my workspace. Main.css is in a folder titled assets. I don't know if that will affect anything...I include the folder name when including the files

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    require_once('inc/template.php');
+
+?>
+
+<html>
+    <head>
+        <title>WhatUpWhale</title>
+        <link rel="stylesheet" href="assets/main.css" type="text/css" />
+    </head>
+    <body>
+        <p class="big">Whatupwhale.com is currently under construction...</p>
+        <p class="small">Please check back soon!</p>
+    </body>
+</html>
